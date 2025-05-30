478057676eb00ba31d6b9cf8c032a89105fc20ef
José Isaac <joseisaacnascimento@gmail.com>
2025-05-29 21:44:56 -0300

    feat: Adiciona página do admin e criar esboço da rota POST para novos equipamentos
    - Substitui variável de equipamentos por um arquivo JSON
    - Cria esboço da rota POST para cadastro de equipamentos
    - Adiciona página do admin

--- src/server.js
+++ src/server.js
@@ -1,46 +1,7 @@
-// teste - lista de equipamentos
-// Lista de equipamentos atualizada e diversificada
-const equipamentos = [
-  {
-    id: 1,
-    nome: "Andaime Tubular",
-    imagem: "/images/image.png",
-    preco: "R$ 35,00 por dia",
-  },
-  {
-    id: 2,
-    nome: "Betoneira 400L",
-    imagem: "/images/image.png",
-    preco: "R$ 120,00 por dia",
-  },
-  {
-    id: 3,
-    nome: "Compactador de Solo",
-    imagem: "/images/image.png",
-    preco: "R$ 150,00 por dia",
-  },
-  {
-    id: 4,
-    nome: "Serra Circular de Bancada",
-    imagem: "/images/image.png",
-    preco: "R$ 90,00 por dia",
-  },
-  {
-    id: 5,
-    nome: "Escora Metálica",
-    imagem: "/images/image.png",
-    preco: "R$ 12,00 por dia",
-  },
-  {
-    id: 6,
-    nome: "Gerador 5kVA",
-    imagem: "/images/image.png",
-    preco: "R$ 200,00 por dia",
-  },
-];
-
 // importa o path
 const path = require("path");
+// importa o fs
+const fs = require("fs");
 // importa o express
 const express = require("express");
 // instancia o express
@@ -49,19 +10,32 @@ const app = express();
 const publicPath = path.join(__dirname, "public");
 // caminho do index.html
 const indexPath = path.join(__dirname, "views", "index.html");
+// caminho do admin.html
+const adminPath = path.join(__dirname, "views", "admin.html");
 
 // middleware para server arquivos estátios da pasta public
 app.use(express.static(publicPath));
 
 // rota principal respondendo com o index.html
 app.get("/", (req, res) => {
-  res.sendFile(indexPath);
+  res.status(200).sendFile(indexPath);
+});
+
+// rota do admin preencher o form de equipamento
+app.get("/admin", (req, res) => {
+  res.status(200).sendFile(adminPath);
 });
 
 // rota para equipamentos
 app.get("/api/equipamentos", (req, res) => {
+  const filePath = path.join(__dirname, "data", "equipamentos.json");
+  const data = fs.readFileSync(filePath, "utf-8");
+  const equipamentos = JSON.parse(data);
   res.json(equipamentos);
 });
+
+// rota post para receber e salvar os dados
+app.post("/api/equipamentos", (req, res) => {});
 
 // porta do servidor
 const port = 3000;
