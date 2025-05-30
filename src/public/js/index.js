a6f1509fca82d202ab377022d8016829e11b71c1
José Isaac <joseisaacnascimento@gmail.com>
2025-05-30 19:33:37 -0300

    feat: Adiciona a seção de contato

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,11 +1,15 @@
+// container de equipamentos
 let containerEquipamentos = document.querySelector(".container-equipamentos");
 
+// função assincrona para consumir a api
 async function consumoApi() {
   // request
   const req = await fetch("/api/equipamentos");
+
   // response em json
   const data = await req.json();
 
+  // para cada equipamento na lista, cria um item
   data.forEach((equipamento) => {
     let item = document.createElement("div");
     item.classList.add("item-equipamento");
@@ -13,17 +17,18 @@ async function consumoApi() {
     <img src="${equipamento.imagemUrl}" alt="${equipamento.nome}">
     <h3>${equipamento.nome}</h3>
     <p>${equipamento.preco}</p>
-    <a 
-      href="https://api.whatsapp.com/send?phone=558695769350&text=Olá,%20tenho%20interesse%20em%20alugar%20o%20produto%20${encodeURIComponent(
-        equipamento.nome
-      )}!"
+    <a href="https://api.whatsapp.com/send?phone=558674001151&text=Olá,%20tenho%20interesse%20em%20alugar%20o%20produto%20${encodeURIComponent(
+      equipamento.nome
+    )}!"
       target="_blank"
       class="btn-whatsapp"
     >
       Solicitar Orçamento
     </a>
   `;
+    // adiciona o item ao container
     containerEquipamentos.appendChild(item);
   });
 }
+// chama a funcao
 consumoApi();
