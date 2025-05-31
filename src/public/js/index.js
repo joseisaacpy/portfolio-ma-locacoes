6399a59b434006429f98129006780effd73bebe9
José Isaac <joseisaacnascimento@gmail.com>
2025-05-31 18:49:46 -0300

    feat: Minifica arquivos de imagens para melhorar desempenho do carregamento

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -16,7 +16,7 @@ async function consumoApi() {
     item.innerHTML = `
     <img src="${equipamento.imagemUrl}" alt="${equipamento.nome}">
     <h3>${equipamento.nome}</h3>
-    <p>${equipamento.preco}</p>
+    <p>Valor: R$${equipamento.preco}</p>
     <a href="https://api.whatsapp.com/send?phone=558674001151&text=Olá,%20tenho%20interesse%20em%20alugar%20o%20produto%20${encodeURIComponent(
       equipamento.nome
     )}!"
