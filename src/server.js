e8885b52e900a97db8b5892d6f7e0b1fc441dd8f
José Isaac <joseisaacnascimento@gmail.com>
2025-06-01 15:44:53 -0300

    feat: Adiciona o middleware de session do express - Valida dados vindo do formulário de cadastro de equipamento via POST

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
+// importa o express
+const express = require("express");
 // importa o path
 const path = require("path");
 // importa o fs
 const fs = require("fs");
-// importa o express
-const express = require("express");
 // instancia o express
 const app = express();
 // caminho da pasta public
@@ -14,23 +14,66 @@ const indexPath = path.join(__dirname, "views", "index.html");
 const adminPath = path.join(__dirname, "views", "admin.html");
 // caminho do json de equipamentos
 const equipamentosPath = path.join(__dirname, "data", "equipamentos.json");
+// importa o express session
+const session = require("express-session");
 
 // middleware para server arquivos estátios da pasta public
 app.use(express.static(publicPath));
+
 // middleware para converter o corpo da requisição em json
 app.use(express.json());
+
+// middleware session para armazenar os dados do login
+app.use(
+  session({
+    secret: "nodeExpress",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 // rota principal respondendo com o index.html
 app.get("/", (req, res) => {
   res.status(200).sendFile(indexPath);
 });
 
-// rota do admin preencher o form de equipamento
-app.get("/admin", (req, res) => {
-  res.status(200).sendFile(adminPath);
+// rota para formulario de login
+app.get("/login", (req, res) => {
+  res.send(`
+      <h2>Login</h2>
+    <form method="POST" action="/login">
+      <input type="text" name="usuario" placeholder="Usuário" required />
+      <input type="password" name="senha" placeholder="Senha" required />
+      <button type="submit">Entrar</button>
+    </form>
+    
+    `);
 });
 
-// rota para equipamentos
+// middleware para ler dados de formulários
+app.use(express.urlencoded({ extended: true }));
+
+// rota de login
+app.post("/login", (req, res) => {
+  const { usuario, senha } = req.body;
+
+  if (usuario === "admin" && senha === "admin") {
+    req.session.usuarioAutenticado = true;
+    return res.redirect("/admin");
+  }
+  res.send("Usuário ou senha inválidos. <a href='/login'>Tentar novamente</a>");
+});
+
+// rota do admin preencher o form de equipamento
+app.get("/admin", (req, res) => {
+  if (!req.session.usuarioAutenticado) {
+    return res.redirect("/login");
+  }
+
+  res.sendFile(adminPath);
+});
+
+// rota para exbir a api de equipamentos
 app.get("/api/equipamentos", (req, res) => {
   // const filePath = path.join(__dirname, "data", "equipamentos.json");
   const data = fs.readFileSync(equipamentosPath, "utf-8");
@@ -38,7 +81,7 @@ app.get("/api/equipamentos", (req, res) => {
   res.json(equipamentos.reverse());
 });
 
-// rota para equipamentos por id
+// rota para exibir equipamentos por id
 app.get("/api/equipamentos/:id", (req, res) => {
   const id = req.params.id;
   const data = fs.readFileSync(equipamentosPath, "utf-8");
@@ -67,9 +110,19 @@ app.delete("/api/equipamentos/:id", (req, res) => {
   res.status(204).send(); // sucesso sem conteúdo
 });
 
-// rota post para receber e salvar os dados
+// rota para receber e salvar um novo equipamento
 app.post("/api/equipamentos", (req, res) => {
   const novoEquipamento = req.body;
+  // valida os dados do equipamento
+  if (
+    !novoEquipamento.nome ||
+    !novoEquipamento.imagemUrl ||
+    !novoEquipamento.preco
+  ) {
+    return res
+      .status(400)
+      .send({ error: "Todos os campos devem ser preenchidos." });
+  }
 
   fs.readFile(equipamentosPath, "utf-8", (err, data) => {
     if (err) return res.status(500).send({ error: "Erro ao ler os dados" });
@@ -97,5 +150,5 @@ app.post("/api/equipamentos", (req, res) => {
 const port = 3000;
 // inicia o servidor
 app.listen(port, () => {
-  console.log("servidor rodando em http://localhost:3000");
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
